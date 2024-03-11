cf3c7d0265fdc1f33dc2b01a6eba91dbfe9110b4
obaraelijah <elijahobara357@gmail.com>
2024-03-11 17:35:32 +0300

    feat: define methods  fns

--- src/main.rs
+++ src/main.rs
@@ -6,27 +6,62 @@ struct Fork;
 
 struct Philosopher {
     name: String,
-    // left_fork: ...
-    // right_fork: ...
-    // thoughts: ...
+    indent: usize,
+    left_fork: Arc<Mutex<Fork>>,
+    right_fork: Arc<Mutex<Fork>>,
+    thoughts: mpsc::Sender<String>,
 }
 
 impl Philosopher {
+    fn print(&self, s: &str) {
+        let indent = " ".repeat(self.indent);
+        println!("{}{}: {}", indent, &self.name, s);
+    }
+    
     fn think(&self) {
+        self.print("is thinking...");
+        random_sleep(10, 30);
         self.thoughts
             .send(format!("Eureka! {} has a new idea!", &self.name))
             .unwrap();
     }
 
     fn eat(&self) {
-        // Pick up forks...
-        println!("{} is eating...", &self.name);
-        thread::sleep(Duration::from_millis(10));
+        let _locks = loop {
+            self.print("trying to get both forks...");
+            let r1 = self.left_fork.try_lock();
+            if r1.is_ok() {
+                self.print("got left fork!");
+            }
+            random_sleep(1, 100);
+            let r2 = self.right_fork.try_lock();
+            if r2.is_ok() {
+                self.print("got right fork!");
+            }
+            if r1.is_ok()  && r2.is_ok() {
+                self.print("got both forks!");
+                break (r1.unwrap(), r2.unwrap());
+            } else {
+                if r1.is_ok() {
+                    self.print("can't get right fork, returning left fork");
+                }
+                if r2.is_ok() {
+                    self.print("can't get left fork, returning right fork");
+                }
+                // releasing  locks that were acquired
+                drop(r1);
+                drop(r2);
+            }
+            random_sleep(1, 1000)
+        };
+        self.print("is eating...");
+        random_sleep(10, 30);
+        self.print("is returning forks...")
     }
 }
 
-static PHILOSOPHERS: &[&str] =
-    &["Socrates", "Hypatia", "Plato", "Aristotle", "Pythagoras"];
+static PHILOSOPHERS: &[&str] = &["Socrates", "Plato", "Aristotle", "Thales", "Pythagoras"];
+
 
 fn main() {
     // Create forks
@@ -36,4 +71,12 @@ fn main() {
     // Make each of them think and eat 100 times
 
     // Output their thoughts
+}
+
+// Utility function to sleep for a random amount of time
+fn random_sleep(a: u64, b: u64) {
+    use rand::Rng;
+    let mut rng = rand::thread_rng();
+    let sleep_time = rng.gen_range(a..=b);
+    thread::sleep(Duration::from_millis(sleep_time));
 }
